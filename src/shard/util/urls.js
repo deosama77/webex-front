618ee1dcaed0cb4e86502c8aa000b4250348ce23
deosama77 <deosama77@gmail.com>
2020-07-28 00:10:37 +0300

    updated

--- src/shard/util/urls.js
+++ src/shard/util/urls.js
@@ -1,2 +1,4 @@
-export const URL_PROJECT_BASE="http://localhost:4001/projects";
-export const URL_USER_BASE="http://localhost:4000/users";
+// export const URL_PROJECT_BASE="http://localhost:4001/projects";
+// export const URL_USER_BASE="http://localhost:4000/users";
+export const URL_PROJECT_BASE=`https://young-beach-29268.herokuapp.com/projects`
+export const URL_USER_BASE=`https://pure-chamber-23521.herokuapp.com/users`;
