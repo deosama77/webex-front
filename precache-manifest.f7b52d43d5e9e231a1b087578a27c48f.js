76f3688780d9fa635716b9f0fd56830734b43c90
deosama77 <deosama77@gmail.com>
2020-07-28 00:16:01 +0300

    Updates

---
+++ precache-manifest.f7b52d43d5e9e231a1b087578a27c48f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdffec9a0450d29ad13e8fb0d1fbda93",
+    "url": "/deosama77/webex-front.git/index.html"
+  },
+  {
+    "revision": "de1f50f158eedcb439b5",
+    "url": "/deosama77/webex-front.git/static/css/main.6f8a6464.chunk.css"
+  },
+  {
+    "revision": "08079dd966d1c7d56cbc",
+    "url": "/deosama77/webex-front.git/static/js/2.0b07abcd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/deosama77/webex-front.git/static/js/2.0b07abcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de1f50f158eedcb439b5",
+    "url": "/deosama77/webex-front.git/static/js/main.7064684e.chunk.js"
+  },
+  {
+    "revision": "cdad92a5afacee840668",
+    "url": "/deosama77/webex-front.git/static/js/runtime-main.fa5ec648.js"
+  }
+]);
